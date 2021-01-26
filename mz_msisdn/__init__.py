23cbe69ec9dcaa6bcaa074682f254cb7639d7c31
mariomthree <mari99manuel@gmail.com>
2021-01-26 12:13:17 +0200

    update of instructions including the method of obtaining the country and network code.

--- mz_msisdn/__init__.py
+++ mz_msisdn/__init__.py
@@ -1,39 +1,77 @@
-def mz_validator(number: str, nr_format: str = "international") -> list:
-    """ Verify if the given number is valid
+"""
+--------------------------------------------------------
+The Validator class.
+This library check numbers, based on the format of Mozambican numbers.
+--------------------------------------------------------
+"""
 
-    Args:
-        number (str): The phone number to be validated
-        nr_format (str, optional): The number has international identification code. Defaults to international.
+class Validator:
 
-    Returns:
-        list: return a list containing all errors identified. If the list is empty it means the number is valid.
-    """
-    number = number.strip()
-    network_codes = ["82", "83", "84", "85", "86", "87"]
-    inter_format = True if nr_format == "international" else False
-    number_size = 12 if inter_format else 9
-    error_messages = []
-
-    error_type = {
-        "size": f"The phone number must be :attr: characters long. currently with {len(number)}.",
-        "format": "The phone number must start with 258.",
-        "network_code": f"Invalid network code. Valid codes: {network_codes}."
+    __error_type = {
+        "size": "The phone number must be 9 or 12 characters long. currently with {}",
+        "format": "The phone number must start with {}.",
+        "network_code": "Invalid network code. {}."
     }
+    
+    __error_messages = []
 
-    if len(number) != number_size:
-        error_messages.append(
-            error_type["size"].replace(":attr:", str(number_size)))
+    __codes =  {
+        "mz": {
+            "network_code": ["82", "83", "84", "85", "86", "87"],
+            "country_code": ["258"]
+        }
+    }
+ 
+    def __init__(self, number, format = "mz"):
+        """
+            Args:
+            number (str): The phone number to be validated
+            format (str, optional): The number has international identification code. Defaults to international.
+        """
+        self.number = str(number)
+        self.format = format
+        self.__validate_number()
+    
+    def get_country_code(self):
+        if len(self.number) == 9:
+            return self.__codes[self.format]["country_code"][0]
+        return self.number[:3] 
 
-    if inter_format:
-        number = number.replace("+", "")
+    def get_network_code(self):
+       if len(self.number) == 9:
+            return self.number[:2]
+       return self.number[3:5] 
+    
+    def get_full_number(self):
+        return self.number
 
-        if "258" not in number[:3]:
-            error_messages.append(error_type["format"])
+    def get_errors(self):
+        return self.__error_messages
 
-        if number[3:5] not in network_codes:
-            error_messages.append(error_type["network_code"])
-    else:
-        if number[:2] not in network_codes:
-            error_messages.append(error_type["network_code"])
+    def __validate_number(self):
+        """ Verify if the number is valid"""
+        
+        self.__remove_signal_plus()
 
-    return error_messages
+        if not self.__is_valid_length():
+           self.__error_messages.append(
+            self.__error_type["size"].format(len(self.number))
+           )
+
+        if self.get_country_code() not in self.__codes[self.format]["country_code"]:
+            self.__error_messages.append(
+            self.__error_type["format"].format(self.__codes[self.format]["country_code"])
+            )
+        
+        if self.get_network_code() not in self.__codes[self.format]["network_code"]:
+           self.__error_messages.append(
+            self.__error_type["network_code"].format(self.__codes[self.format]["network_code"])
+           )
+    
+    def __is_valid_length(self):
+        if len(self.number) in [9, 12]:
+            return True
+        return False
+    
+    def __remove_signal_plus(self):
+        self.number = self.number.strip().replace("+", "")    
